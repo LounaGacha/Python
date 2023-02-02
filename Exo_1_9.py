321b7c0beab7a6fcd7ce878261f77c8409074631
LounaGacha <113609816+LounaGacha@users.noreply.github.com>
2023-02-02 10:26:38 +0100

    Update Exo_1_9.py

--- Exo_1_9.py
+++ Exo_1_9.py
@@ -1,2 +1,14 @@
-n_couple =0
-eq = str(a)*2 * str(b)*2
+for a in range(1,10):
+    aa = (10*a) + a
+    for b in range(1,10):
+        bb = (10*b) +b
+        res = aa * bb
+        c1 = res // 1000
+        reste = res % 1000
+        c2 = reste // 100
+        reste = reste % 100
+        if c1 == c2:
+            d1 = reste // 10
+            d2 = reste % 10
+            if d1 == d2:
+                print(aa," * ",bb," = ",res)
